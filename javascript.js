a99ff7a08d0dea13813214546275b41459ef7f5d
abertjames <96638271+abertjames@users.noreply.github.com>
2022-04-01 18:11:50 -0400

    update rock paper scissors to include basic ui
    game reports a winner after 5 rounds have been won and
    now uses buttons instead of text input

--- javascript.js
+++ javascript.js
@@ -22,42 +22,103 @@ function computerPlay() {
 // console.log(playerSelection);
 // console.log(computerSelection);
 
-let roundWinner;
+// let roundWinner;
+// function playRound(playerSelection,computerSelection) {
+//     if (playerSelection === "rock" && computerSelection === "paper") {
+//         return roundWinner = "Computer Wins"
+//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//         return roundWinner = "Computer Wins"
+//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//         return roundWinner = "Computer Wins"
+//     } else if (playerSelection === computerSelection) {
+//         return roundWinner = "Tie"
+//     } else
+//      return roundWinner = "Player Wins"
+// }
+
+// function game() {
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     let tie = 0;
+
+//     for (let i = 0; i < 5; i++) {
+//         playRound(playerSelect(),computerPlay());
+//         if (roundWinner == "Computer Wins") {
+//             computerWins += 1;
+//         } else if (roundWinner == "Player Wins") {
+//             playerWins += 1;
+//         } else if (roundWinner == "Tie") {
+//             tie += 1;
+//         }
+        
+//     }
+//     return (`Player won: ${playerWins}, Computer won: ${computerWins}, Ties: ${tie}`)
+// }
+
+let playerWins = 0;
+let computerWins = 0;
+let tie = 0;
 function playRound(playerSelection,computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return roundWinner = "Computer Wins"
+        return computerWins +=1
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return roundWinner = "Computer Wins"
+        return computerWins +=1
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return roundWinner = "Computer Wins"
+        return computerWins +=1
     } else if (playerSelection === computerSelection) {
-        return roundWinner = "Tie"
+        return tie +=1
     } else
-     return roundWinner = "Player Wins"
+        return playerWins +=1
 }
 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    let tie = 0;
 
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelect(),computerPlay());
-        if (roundWinner == "Computer Wins") {
-            computerWins += 1;
-        } else if (roundWinner == "Player Wins") {
-            playerWins += 1;
-        } else if (roundWinner == "Tie") {
-            tie += 1;
-        }
-        
-    }
-    return (`Player won: ${playerWins}, Computer won: ${computerWins}, Ties: ${tie}`)
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    // alert("Rock");
+    playRound("rock",computerPlay());
+    updateScore()
+});
 
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+//   alert("Paper");
+    playRound("paper",computerPlay());
+    updateScore()
+});
 
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+//   alert("Scissors");
+    playRound("scissors",computerPlay());
+    updateScore()
+});
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    computerWins = playerWins = tie = 0;
+    updateScore()
+});
+
+const results = document.querySelector('#results');
+const score = document.createElement('div');
+score.classList.add('score');
+
+function updateScore (){
+    if (computerWins == 5) {
+        score.textContent = `The score is computer: ${computerWins}, player: ${playerWins},
+        Ties: ${tie}. The Computer Won`;
+    } else if (playerWins == 5) {
+        score.textContent = `The score is computer: ${computerWins}, player: ${playerWins},
+        Ties: ${tie}. You Won`;
+    } else 
+    score.textContent = `The score is computer: ${computerWins}, player: ${playerWins},
+    Ties: ${tie}`;
 }
+updateScore()
+results.appendChild(score);
+
 
 // alert (playRound(playerSelection, computerSelection));
 
-alert (game())
+// alert (game())
 
